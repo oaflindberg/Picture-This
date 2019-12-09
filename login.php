ebb652c46d556409338eff39ab3d7ebdc419b3e3
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2019-12-09 15:10:10 +0100

    Added background image to index.php and login.php

--- login.php
+++ login.php
@@ -1,23 +1,27 @@
-<!-- <?php require __DIR__.'/views/header.php'; ?> -->
+<img class="background-image" src="assets/images/blue-orange.jpeg" alt="picture of a blue orange">
 
+<?php 
+require __DIR__.'/views/header.php';
+?>
+
+<div class="login-container">
 <article class="login">
     <h1>Login</h1>
 
     <form action="app/users/login.php" method="post">
-        <div>
+        <div class="form-section">
             <label for="email">Email</label>
-            <input type="email" name="email" placeholder="francis@darjeeling.com" required>
-            <small>Please provide the your email address.</small>
+            <input class="input" type="email" name="email" placeholder="example@email.com" required>
         </div>
 
-        <div>
+        <div class="form-section">
             <label for="password">Password</label>
-            <input type="password" name="password" required>
-            <small>Please provide the your password (passphrase).</small>
+            <input class="input" type="password" name="password" required>
         </div>
 
         <button type="submit">Login</button>
     </form>
 </article>
+</div>
 
 <?php require __DIR__.'/views/footer.php'; ?>
