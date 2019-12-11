a6a26a58b6f0e2e40697e8e0ba7e7f1d7ce7b216
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2019-12-11 20:26:37 +0100

    Fixed styling of account page

--- account.php
+++ account.php
@@ -11,13 +11,33 @@ declare(strict_types=1); ?>
 
 <div class="account-container">
 
+
+    <section class="posts-wrapper">
+
+        <h1 class="posts-header">Posts by you</h1>
+        <div class="posts-container">
+
+            <?php foreach ($posts as $post) : ?>
+                <div class="posts" data-id="<?php echo $post['id']; ?>">
+                    <img src="/uploads/posts/<?php echo $post['image']; ?>" alt="" height="150px" width="auto">
+                    <p><?php echo $post['caption']; ?></p>
+                </div>
+                </a>
+            <?php endforeach; ?>
+
+        </div>
+    </section>
+
+
+
+    <h1 class="account-settings-header">Account settings</h1>
     <section class="bio-change">
         <h2>Change biography</h2>
         <form class="form-section" action="/app/users/account.php" method="post">
             <label for="biography">Biography</label>
-            <input type="text" name="biography" id="biography" value="<?php $biography["biography"]; ?>">
+            <input class="biography-input" type="text" name="biography" id="biography" value="<?php $biography["biography"]; ?>">
             <div class="btns-container">
-                <button type="submit">Update</button>
+                <button class="account-btn" type="submit">Update</button>
             </div>
         </form>
     </section>
@@ -25,11 +45,11 @@ declare(strict_types=1); ?>
         <h2>Change email</h2>
         <form class="form-section" action="/app/users/account.php" method="post">
             <label for="oldemail">Current email</label>
-            <input type="email" name="oldemail" id="oldemail">
+            <input class="account-input" type="email" name="oldemail" id="oldemail">
             <label for="email">New email</label>
-            <input type="email" name="newemail" id="newemail">
+            <input class="account-input" type="email" name="newemail" id="newemail">
             <div class="btns-container">
-                <button type="submit">Reset</button>
+                <button class="account-btn" type="submit">Reset</button>
             </div>
         </form>
     </section>
@@ -38,35 +58,18 @@ declare(strict_types=1); ?>
         <h2>Change password</h2>
         <form class="form-section" action="/app/users/account.php" method="post">
             <label for="oldpassword">Current password</label>
-            <input type="password" name="oldpassword" id="oldpassword">
+            <input class="account-input" type="password" name="oldpassword" id="oldpassword">
             <label for="password">New password</label>
-            <input type="password" name="password" id="password">
+            <input class="account-input" type="password" name="password" id="password">
             <label for="passwordrepeat">Confirm new password</label>
-            <input type="password" name="passwordconfirm" id="passwordconfirm">
+            <input class="account-input" type="password" name="passwordconfirm" id="passwordconfirm">
 
             <div class="btns-container">
-                <button type="submit">Reset</button>
-                <button class="logout"><a href="/app/users/logout.php">Log out</a></button>
+                <button class="account-btn" type="submit">Reset</button>
+                <button class="account-btn" class="logout"><a href="/app/users/logout.php">Log out</a></button>
             </div>
         </form>
     </section>
-
-    <h1>Posts by you</h1>
-    <div class="posts-container">
-
-        <?php foreach ($posts as $post) : ?>
-            <div class="posts" data-id="<?php echo $post['id']; ?>">
-                <img src="/uploads/posts/<?php echo $post['image']; ?>" alt="" height="150px" width="auto">
-                <p><?php echo $post['caption']; ?></p>
-            </div>
-            </a>
-        <?php endforeach; ?>
-
-    </div>
-
-</div>
-</section>
-
 </div>
 
 
