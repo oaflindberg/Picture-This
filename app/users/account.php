bad10addbfb480707f145136b8ad57357ae1690f
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-01-15 15:41:05 +0100

    PLZ WORK (ノ⁠ಠ⁠益⁠ಠ)⁠ノ⁠彡⁠┻━┻

--- app/users/account.php
+++ app/users/account.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 require __DIR__ . '/../autoload.php';
 
 
-/**
- *  UPDATE PASSWORD
- */
+//  UPDATE PASSWORD
+
 if (isset($_POST['oldpassword'], $_POST['password'], $_POST['passwordconfirm'])) {
     $oldPwd = $_POST['oldpassword'];
     $newPwd = $_POST['password'];
@@ -35,9 +34,8 @@ if (isset($_POST['oldpassword'], $_POST['password'], $_POST['passwordconfirm']))
     }
 }
 
-/**
- *  UPDATE EMAIL
- */
+//  UPDATE EMAIL
+
 if (isset($_POST['oldemail'], $_POST['newemail'])) {
     $oldEmail = filter_var($_POST['oldemail'], FILTER_SANITIZE_EMAIL);
     $oldValidEmail = filter_var($_POST['oldemail'], FILTER_VALIDATE_EMAIL);
@@ -65,9 +63,8 @@ if (isset($_POST['oldemail'], $_POST['newemail'])) {
     }
 }
 
-/**
- *  UPDATE BIOGRAPHY
- */
+//  UPDATE BIOGRAPHY
+
 if (isset($_POST['biography'])) {
     $biography = trim(filter_var($_POST['biography'], FILTER_SANITIZE_STRING));
 
@@ -88,33 +85,34 @@ if (isset($_POST['biography'])) {
     redirect('/account.php');
 }
 
-/**
- *  UPDATE AVATAR
- */
+//  UPDATE AVATAR
+
 if (isset($_FILES['profilepicture'])) {
 
     $avatar = $_FILES['profilepicture'];
 
-    $filename = 'avatar' . '-' . date('ymdsu') . '.png';
+    if (!empty($avatar)) {
+        $filename = 'avatar' . '-' . date('ymdsu') . '.png';
 
-    $destination = __DIR__ . '/../../uploads/avatar/' . $filename;
+        $destination = __DIR__ . '/../../uploads/avatar/' . $filename;
 
-    move_uploaded_file($avatar['tmp_name'], $destination);
+        move_uploaded_file($avatar['tmp_name'], $destination);
 
-    $statement = $pdo->prepare('SELECT * FROM users WHERE id = :id');
-    $statement->execute([
-        ':id' => $_SESSION['user']['id']
-    ]);
-    $user = $statement->fetch(PDO::FETCH_ASSOC);
+        $statement = $pdo->prepare('SELECT * FROM users WHERE id = :id');
+        $statement->execute([
+            ':id' => $_SESSION['user']['id']
+        ]);
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
 
 
-    $newAvatar = $pdo->prepare('UPDATE users SET avatar = :newavatar WHERE id = :id');
-    $newAvatar->execute([
-        ':id' => $_SESSION['user']['id'],
-        ':newavatar' => $filename,
-    ]);
-    redirect('/account.php');
-} else {
-    $_SESSION['noAvatar'] = 'Please select a file to update your avatar';
-    redirect(('/settings.php'));
+        $newAvatar = $pdo->prepare('UPDATE users SET avatar = :newavatar WHERE id = :id');
+        $newAvatar->execute([
+            ':id' => $_SESSION['user']['id'],
+            ':newavatar' => $filename,
+        ]);
+        redirect('/account.php');
+    } else {
+        $_SESSION['noAvatar'] = 'Please select a file to update your avatar';
+        redirect('/settings.php');
+    }
 }
